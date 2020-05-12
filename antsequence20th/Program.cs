a0a829b362455eda291e27570a2ceea211296684
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-12 14:48:28 +0900

    초기 커밋.

---
+++ antsequence20th/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antsequence20th
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String num = "1";
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(i + 1 + "번째 수열: " + num);
+                char n = num[0];
+                int c = 0;
+                String num2 = "";
+
+                for (int j = 0; j < num.Length; j++)
+                {
+                    if (n == num[j])
+                        c++;
+                    else
+                    {
+                        num2 += n;
+                        num2 += c;
+                        n = num[j];
+                        c = 1;
+                    }
+                    if (j + 1 == num.Length)
+                    {
+                        num2 += n;
+                        num2 += c;
+                    }
+                }
+                num = num2;
+            }
+        }
+    }
+}
